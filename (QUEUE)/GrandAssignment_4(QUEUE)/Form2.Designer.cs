080aabeda24211faad73f4b8c70fe10cd95eb4f0
Omer Khan <omerkhan842@gmail.com>
2018-08-13 11:28:31 +0500

    Queue

---
+++ (QUEUE)/GrandAssignment_4(QUEUE)/Form2.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace GrandAssignment_4_QUEUE_
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.FPtr = new System.Windows.Forms.Label();
+            this.RPtr = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(225, 148);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(555, 108);
+            this.panel1.TabIndex = 0;
+            // 
+            // FPtr
+            // 
+            this.FPtr.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.FPtr.Location = new System.Drawing.Point(148, 156);
+            this.FPtr.Name = "FPtr";
+            this.FPtr.Size = new System.Drawing.Size(45, 38);
+            this.FPtr.TabIndex = 1;
+            // 
+            // RPtr
+            // 
+            this.RPtr.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.RPtr.Location = new System.Drawing.Point(148, 206);
+            this.RPtr.Name = "RPtr";
+            this.RPtr.Size = new System.Drawing.Size(45, 38);
+            this.RPtr.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Front Pointer:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Rare Pointer:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(225, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Enqueue";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(239, 295);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(48, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(186, 296);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Value:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(366, 296);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "DEQUEUE:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(333, 333);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Serve";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(414, 333);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Leave";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Monotype Corsiva", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label5.Location = new System.Drawing.Point(326, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 39);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "QUEUE:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(695, 293);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Queue Full";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(695, 322);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Queue Empty";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(695, 351);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(85, 23);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "Current Size";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 447);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RPtr);
+            this.Controls.Add(this.FPtr);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form2";
+            this.Text = "QUEUE";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label FPtr;
+        private System.Windows.Forms.Label RPtr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
---
+++ (QUEUE)/GrandAssignment_4(QUEUE)/Form2.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GrandAssignment_4_QUEUE_
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        int[] arr;
+        int index = 9;
+        int rarePtr;
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            CreateQueue();
+            arr = new int[10];
+            FPtr.Text = "0";
+            RPtr.Text = "null";
+            FPtr.Font = new Font("Microsoft Sans Serif", 9, FontStyle.Bold);
+            RPtr.Font = new Font("Microsoft Sans Serif", 9, FontStyle.Bold);
+        }
+
+        public void CreateQueue()
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Label lbl = new Label();
+                    lbl.Font = new Font("Arial", 9, FontStyle.Bold);
+                    lbl.Name = "lbl" + j;
+                    //  lbl.Text = (j + 1).ToString();
+                    lbl.Size = new Size(40, 80);
+                    lbl.Location = new Point(j * 40, i * 40);
+                    lbl.BorderStyle = BorderStyle.FixedSingle;
+                    panel1.Controls.Add(lbl);
+
+
+                }
+            }
+        }
+
+        public void PrintQueue()
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Label lbl = panel1.Controls.Find("lbl" + j, true).FirstOrDefault() as Label;
+                    lbl.Text = arr[j].ToString();
+
+                }
+            }
+        }
+
+        public bool ArrayFull(int[] num)
+        {
+            if ((num[num.Length - 1] > 0 || num[num.Length - 1] < 0) && (num[0] > 0 || num[0] < 0) && (num[num.Length - 2] > 0 || num[num.Length - 2] < 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool ArrayEmpty(int[] num)
+        {
+            int count = 0;
+            for (int i = 0; i != num.Length; i++)
+            {
+                if (num[i] == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == num.Length)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        public int[] shiftRight(int[] num)
+        {
+            int count = 0;
+
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                if (num[i] > 0 || num[i] < 0)
+                {
+                    count = i;
+                    break;
+                }
+            }
+
+            for (int i = count; i >= 0; i--)
+            {
+                num[count + 1] = num[count];
+
+                count--;
+            }
+
+            return num;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool full = ArrayFull(arr);
+            if (full)
+            {
+                MessageBox.Show("Queue Full!");
+            }
+            else
+            {
+                bool r = int.TryParse(textBox1.Text, out arr[index]);
+                if (!r)
+                {
+                    MessageBox.Show("Invalid value entered!");
+                }
+                else
+                {
+                    FPtr.Text = "1";
+                    rarePtr = 10 - index;
+                    RPtr.Text = rarePtr.ToString();
+                    index--;
+                    PrintQueue();
+                    textBox1.Clear();
+                    textBox1.Focus();
+
+                }
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool empty = ArrayEmpty(arr);
+            if (empty)
+            {
+                MessageBox.Show("Cannot Serve anything from an empty Queue!");
+            }
+            else
+            {
+                arr[9] = 0;
+                arr = shiftRight(arr);
+                arr[0] = 0;
+                rarePtr--;
+                RPtr.Text = rarePtr.ToString();
+                index++;
+                PrintQueue();
+                if (RPtr.Text == "0")
+                {
+                    FPtr.Text = "0";
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bool empty = ArrayEmpty(arr);
+            if (empty)
+            {
+                MessageBox.Show("Cannot Leave anything from an empty Queue!");
+            }
+            else
+            {
+                arr[index+1] = 0;
+                rarePtr--;
+                RPtr.Text = rarePtr.ToString();
+                index++;
+                PrintQueue();
+                if (RPtr.Text == "0")
+                {
+                    FPtr.Text = "0";
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (RPtr.Text == "10")
+            {
+                MessageBox.Show("Queue is Full");
+            }
+            else
+            {
+                MessageBox.Show("Queue is not Full");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (FPtr.Text == "0")
+            {
+                MessageBox.Show("Queue is Empty");
+            }
+            else
+            {
+                MessageBox.Show("Queue is not Empty");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Currently Queue has " + RPtr.Text + " values");
+        }
+
+
+    }
+}
